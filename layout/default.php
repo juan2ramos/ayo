ff71ebc232bd6bbbf9e5a9ada415d66f55796264
Juan <juan2ramos@gmail.com>
2015-09-16 09:29:41 -0500

    maps

--- layout/default.php
+++ layout/default.php
@@ -132,7 +132,7 @@
 <script src="js/owl.js"></script>
 <script src="js/script.js"></script>
 
-<?php if ($url == '/contacto'): ?>
+<?php if ($url == 'contacto'): ?>
     <script src="js/map.js"></script><?php endif ?>
 <!-- BEGIN JIVOSITE CODE {literal} -->
 <script type='text/javascript'>
